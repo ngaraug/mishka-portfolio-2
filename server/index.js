ef395fabf68a95ae4b3453f8475fd2377c94e228
ngaraug <gaurangbmestry3@gmail.com>
2024-02-14 12:15:24 +0530

    Connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = process.env.MONGOURI;
+
+mongoose.connect(uri)
+    .then( 
+        () => console.log("Connected to BD")
+    ).catch(
+        (err) => console.log(err)
+    )
 
 const app = express();
 
 app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3000!!!");
 })
