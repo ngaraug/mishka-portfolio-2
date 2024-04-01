b7281b43a95da8d154013b764542256afa2eced5
ngaraug <gaurangbmestry3@gmail.com>
2024-04-02 02:23:55 +0530

    blogs page basic layout and temp blogs add

--- client/src/pages/BlogsPage.jsx
+++ client/src/pages/BlogsPage.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Blog from '../components/Blog.jsx'
+
+import blogs from '../../public/Blogs/blogs.js'
 
 export default function BlogsPage() {
   return (
     <>
       <NavBar />
-      Blogs
+      <div id="main-container">
+
+      <div className='page-container'>
+          <div className='page-contents'>
+            <h1 className='page-title'> <span className='splletters'>Blogs  </span>by Mishka </h1>
+            <div id='all-blogs-container'>
+              {
+                blogs.map((data) =>{
+                  return <Blog key={data.id} title={data.title} shortTitle={data.shortTitle} body={data.body}/>
+                })
+              }
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
