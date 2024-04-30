e4547c38fa90f1f09824e22b36d63fdd7424d34d
ngaraug <gaurangbmestry3@gmail.com>
2024-04-30 18:47:22 +0530

    Admin desk bug fix and component templates add for the same

---
+++ client/src/components/adminComponents/AdminNavBar.jsx
@@ -0,0 +1,22 @@
+export default function AdminNavBar() {
+  return (
+    <>
+
+    <nav className="nav">
+        <div className="container">
+            <div className="logo">
+                <a href="/" id='home-logo-link'><img id='mishka-logo' src="/images/transparent-logo-1.png" alt="" /></a>
+            </div>
+        </div>
+        <div className="admin-navbar-title">
+            <div>Admin </div>
+            <div>Desk</div>
+        </div>
+    </nav>
+    
+    </>
+  )
+}
+
+
+
--- client/src/pages/admin/AdminHome.jsx
+++ client/src/pages/admin/AdminHome.jsx
@@ -1,24 +1,50 @@
 import React from 'react'
-import NavBar from '../../components/NavBar'
+import AdminNavBar from '../../components/adminComponents/AdminNavBar'
+import { useState, useEffect } from 'react'
+import AdminArticlesDesk from '../../components/adminComponents/AdminArticlesDesk'
+import AdminBlogsDesk from '../../components/adminComponents/AdminBlogsDesk'
+import AdminReviewsDesk from '../../components/adminComponents/AdminReviewsDesk'
 
 export default function AdminHome() {
+
+  const [pageState, setPageState] = useState('blogs')
+  const handlePages = (e) => {
+    setPageState(e.target.id)   
+  }
   return (
     <>
-        <NavBar />
+        <AdminNavBar />
         <div id="admin-main-container">
+            {/* Page state selector section*/}
             <div className='admin-home-header'>
-                <h1 className='admin-home-heading'>Admin Desktop</h1>
                 <div className='admin-home-selector-holder'>
-                    <input type="radio" name="content-type" id="articles" className='hidden admin-content-selector'/>
-                    <input type="radio" name="content-type" id="blogs" className='hidden admin-content-selector'/>
-                    <input type="radio" name="content-type" id="reviews" className='hidden admin-content-selector'/>
-
-                    <label htmlFor="articles" className='admin-content-selector-label'>My Articles</label>
-                    <label htmlFor="blogs" className='admin-content-selector-label'>My Blogs</label>
-                    <label htmlFor="reviews" className='admin-content-selector-label'>My Reviews</label>
+                    <button id='blogs' className={pageState === 'blogs' ? 'admin-blog-selector admin-content-selector-label admin-content-selector-label-active' : 'admin-blog-selector admin-content-selector-label'} onClick={handlePages}>
+                      Blogs
+                    </button>
+                    <button id='articles' className={pageState === 'articles' ? 'admin-blog-selector admin-content-selector-label admin-content-selector-label-active' : 'admin-blog-selector admin-content-selector-label'}  onClick={handlePages}>
+                      Articles
+                    </button>
+                    <button id='reviews' className={pageState === 'reviews' ? 'admin-blog-selector admin-content-selector-label admin-content-selector-label-active' : 'admin-blog-selector admin-content-selector-label'}  onClick={handlePages}>
+                      Reviews
+                    </button>
+                    
                 </div>
             </div>
+            {
+              pageState === 'blogs' ? (
+                <AdminBlogsDesk />
+              ) : pageState === 'articles' ?(
+                <AdminArticlesDesk />
+              ) : pageState === 'reviews' ? (
+                <AdminReviewsDesk />
+              ) : (
+                <div>
+                  Please select a page from above 👆
+                </div>
+              )
+            }
         </div>
     </>
   )
 }
+
